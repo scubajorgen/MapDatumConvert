1d469207093e445ee6f98c1396f69cf91caad1bb
jorgen <scubajorgen@gmail.com>
2022-10-26 08:37:49 +0200

    Code javadoc added

--- src/main/java/net/studioblueplanet/mapdatumconvert/MapProjection.java
+++ src/main/java/net/studioblueplanet/mapdatumconvert/MapProjection.java
@@ -11,6 +11,17 @@ package net.studioblueplanet.mapdatumconvert;
  */
 public interface MapProjection
 {
+    /**
+     * Converts a latitude/longitude in degrees to easting, northing in m
+     * @param latlon The latitude/longitude coordinate
+     * @return The easting/northing coordinate
+     */
     public DatumCoordinate          latLonToMapDatum(LatLonCoordinate latlon);
+    
+    /**
+     * Converts an easting/northing coordinate to a latitude/longitude coordinate.
+     * @param md Easting/northing coordinate in meter
+     * @return Latitude/Longitude in degrees
+     */
     public LatLonCoordinate         mapDatumToLatLon(DatumCoordinate  md);
 }
